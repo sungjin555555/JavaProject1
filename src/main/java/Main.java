162349669f446d0323df7bb0226de1b08f57279b
오성진 <oseongjin@oseongjin-ui-MacBookPro.local>
2022-09-06 16:17:32 +0900

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world!!");
+    public static void main(String[] args) {
+
+        new WordManager().start();
     }
 }
