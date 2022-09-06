4910e05314ab1e892a26c84c680698653d844fc8
오성진 <oseongjin@oseongjin-ui-MacBookPro.local>
2022-09-06 16:32:31 +0900

    Commit 2

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -36,4 +36,12 @@ public class Word {
     public void setMeaning(String meaning) {
         this.meaning = meaning;
     }
+
+    public String toString(){
+
+        String slevel = "";
+        for(int i = 0; i < level; i++) slevel += "*";
+        String str = String.format("%3s", slevel) + String.format("%15s", word) + "  " + meaning;
+        return str;
+    }
 }
